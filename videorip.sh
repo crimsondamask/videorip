2b23cb4925f8c8b7bb1ef027f0a1956e8864dab6
crimsondamask <abdelkadermadoui@gmail.com>
2021-06-29 12:49:05 +0100

    Removed the if block

--- videorip.sh
+++ videorip.sh
@@ -3,23 +3,19 @@
 [ ! -f "$2" ] && printf "USAGE:\n./videorip VIDEO TIMESTAMPS DELIMITER" && exit 1 
 [ ! -f "$1" ] && printf "USAGE:\n./videorip VIDEO TIMESTAMPS DELIMITER" && exit 1 
 
-#Parsing arguments and cleaning the filename
-
 timestamps="$2"
 delimiter="$3"
 filename=$(basename -- "$1")
 ext="${filename##*.}"
-#ext="m4a"
 filename=${filename%.*}
 safename="$(echo $filename | iconv -cf UTF-8 -t ASCII//TRANSLIT | tr -d '[:punct:]' | tr '[:upper:]' '[:lower:]' | tr ' ' '-' | sed "s|\+|-|g;s|\|||g;s|\$||g;s|&||g")"
 
-#creating a directory for output files
 directory="$safename"
 mkdir -p $directory
 
 #Getting the total number of lines to control the loop
 
-linecount="$( cat $2 | wc -l )"
+linecount="$( wc -l < $2 )"
 echo "$linecount sections found."
 
 COUNT=0
@@ -34,12 +30,13 @@ do
     TIME=$(echo $line | tr -s ' ' | cut -d ' ' -f 1)
 
     #Check if the timing is in a H:M:S format
-    if [ "$( echo "$TIME" | grep -o ':' | wc -l )" -eq 1 ]
-    then
-        TIMESAFE=$( date -d "00:$TIME" +"%T" )
-    else
-        TIMESAFE=$(date -d "$TIME" +"%T")
-    fi
+
+    [ "$( echo "$TIME" | grep -o ':' | wc -l )" -eq 1 ] && 
+        TIMESAFE=$( date -d "00:$TIME" +"%T" ) || 
+        TIMESAFE=$(date -d "$TIME" +"%T") || exit 1 
+
+    #Getting chapter titles.
+
     word="$(echo $line | tr -s ' ' | awk -F "$delimiter" '{print $2, $3, $4, $5, $6}' | sed "s|\ ||g;s|\&||g;s|\$||g;s|\#||g;s|\?||g;s|\/||g;s|\|||g")"
     echo $word
     sections+=("${word}")
@@ -55,8 +52,8 @@ do
     echo "Slicing from ${stamps[${COUNT}]} to ${stamps[$(( COUNT + 1 ))]} ====> $title"
 
     #For audio output add the -vn option 
-    ffmpeg -nostdin -y -loglevel -8 -i $1 -ss "${stamps[$COUNT]}" -c copy -to "${stamps[$(( COUNT +1 ))]}" "$directory/$title.$ext" &&
-    COUNT=$((COUNT+1))
+    ffmpeg -nostdin -y -loglevel -8 -i $1 -ss "${stamps[$COUNT]}" -c copy -to "${stamps[$(( COUNT + 1 ))]}" "$directory/$title.$ext" && echo "YES"
+    COUNT=$(( COUNT+1 ))
 done
 
 #The last slice is done outside the loop
